c917e1eaba141bf95e83706d02b3addcb8fb225e
JustinSilvas <98841304+JustinSilvas@users.noreply.github.com>
2022-03-22 12:31:31 -0400

    Platform fix

--- BigProgect2DPlatformer/Assets/Platformenemy.cs
+++ BigProgect2DPlatformer/Assets/Platformenemy.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class Platformenemy : MonoBehaviour
 {
-    public GameObject play;
-    private Rigidbody2D body;
-    public Transform player;
-    public Transform firingPoint;
+    public GameObject play; //Player alive trackiong
+    private Rigidbody2D body; //Rigidbody of enemy
+    public Transform player; //Player location
+    public Transform firingPoint; 
     public GameObject bulletPrefabRight;
     public GameObject bulletPrefabLeft;
 
     public float distance;
     private float range = 10;
-    private float walkingSpeed = 5;
+    private float walkingSpeed = 5; //Set it to negative
     private bool inRange = false;
     private float checkDist;
-    public bool direction = false;
+    public bool direction = false; //False = Left and True = Right
     private float shootCooldown = 5;
     private float nextShot;
     private bool Shot;
-    private int health = 30;
+    private int health;
 
     // Start is called before the first frame update
     void Start()
     {
+        health = 30;
         body = GetComponent<Rigidbody2D>();
         body.velocity = new Vector2(walkingSpeed, 0);
         transform.localScale = new Vector3(-1, 1, 1);
@@ -34,29 +35,33 @@ public class Platformenemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        nextShot += Time.deltaTime;
-       
-
-        if (nextShot > shootCooldown)
+        if (play != null)
         {
-            Shot = true;
-        }
+            nextShot += Time.deltaTime;
 
-        if (Shot)
-        {
-            DistanceCheck();
-            if (inRange)
+            //Shot coolDown
+            if (nextShot > shootCooldown)
             {
-                Throw();
-                Shot = false;
-                nextShot = 0;
+                Shot = true;
             }
-            
+
+            if (Shot)
+            {
+                DistanceCheck();
+                if (inRange)
+                {
+                    Throw();
+                    Shot = false;
+                    nextShot = 0;
+                }
+
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //Bullet Collision
         if (collision.gameObject.CompareTag("Bullet"))
         {
             health -= 10;
@@ -76,29 +81,28 @@ public class Platformenemy : MonoBehaviour
 
     private void DistanceCheck()
     {
-        if (play != null)
+        
+        distance = player.position.x - body.position.x;
+        if (distance < range && distance > -range)
         {
-            distance = player.position.x - body.position.x;
-            if (distance < range && distance > -range)
+            if (distance > 0)
             {
-                if (distance > 0)
-                {
-                    transform.localScale = Vector3.one;
-                    direction = true;
+                transform.localScale = Vector3.one;
+                direction = true;
 
-                }
-                else if (distance < 0)
-                {
-                    transform.localScale = new Vector3(-1, 1, 1);
-                    direction = false;
-                }
-                inRange = true;
             }
-            else
+            else if (distance < 0)
             {
-                inRange = false;
+                transform.localScale = new Vector3(-1, 1, 1);
+                direction = false;
             }
+            inRange = true;
         }
+        else
+        {
+            inRange = false;
+        }
+        
     }
 
     private void Throw()
