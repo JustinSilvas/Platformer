a93429da53a4b47be67a40036c684f9bd7754bc6
NHaddock21 <98841101+NHaddock21@users.noreply.github.com>
2022-03-29 13:40:31 -0400

    Update WalkingEnemyWall.cs

--- BigProgect2DPlatformer/Assets/WalkingEnemyWall.cs
+++ BigProgect2DPlatformer/Assets/WalkingEnemyWall.cs
@@ -6,18 +6,20 @@ using UnityEngine.UI;
 
 public class WalkingEnemyWall : MonoBehaviour
 {
-    public GameObject play;    //Player alive track
-    private Rigidbody2D body;    //Rigidbody of enemy
-    public Transform player;     //player location track
-    private Bullet b;     //Bullet reference
+    public GameObject play;
+    private Rigidbody2D body;
+    public Transform player;
+    private Bullet b;
     private float range = 10;
     private float walkingSpeed = -5;
     private bool wallCollision = false;
-    [SerializeField] float rightWallLimit = 29;
     private bool inRange = false;
     private float checkDist;
-    private bool direction = false;     //False = Left and True = right
+    private bool direction = false;
     public float health = 10;
+
+    bool facingLeft;
+
     void Awake()
     {
         body = GetComponent<Rigidbody2D>();
@@ -28,56 +30,55 @@ public class WalkingEnemyWall : MonoBehaviour
 
     private void LateUpdate()
     {
-        //Check if player is null and if not dont run
-        if (play != null)
+        body.velocity = new Vector2(walkingSpeed, 0);
+        if (wallCollision && direction == false)
         {
-            //Set walking speed
             body.velocity = new Vector2(walkingSpeed, 0);
-            // Wall collision and direction to left
-            if (wallCollision && direction == false)
-            {
-                walkingSpeed = -walkingSpeed;
-                wallCollision = false;
-                direction = true;
-            }
-            //If passed x limit and direction right
-            if (body.position.x >= rightWallLimit && direction == true)
-            {
-                walkingSpeed = -walkingSpeed;
-                direction = false;
-            }
-            //Checks y value to check distance
-            if (player.position.y <= body.position.y + 2)
-            {
-                DistanceCheck();
-            }    
-            if (body.velocity.x > 0 && !facingLeft)
-            {
+            walkingSpeed = -walkingSpeed;
+            wallCollision = false;
+            direction = true;
+            
+        }
+        if (body.position.x >= 29 && direction == true)
+        {
+            walkingSpeed = -walkingSpeed;
+            body.velocity = new Vector2(walkingSpeed, 0);
+            direction = false;
+            
+        }
+        if (player.position.y <= body.position.y + 2)
+        {
+            DistanceCheck();
+        }
+        
+        if (transform.position.y < -8)
+        {
+            Destroy(this.gameObject);
+        }
+        if (body.velocity.x > 0 && !facingLeft)
+        {
             facingLeft = true;
             body.transform.Rotate(0, 180, 0);
-            }
+        }
         else if (body.velocity.x < 0 && facingLeft)
-            {
+        {
             facingLeft = false;
             body.transform.Rotate(0, 180, 0);
-            }
         }
+        
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Wall Collision Check
         if (collision.gameObject.CompareTag("Wall"))
         {
             wallCollision = true;
         }
 
-        //Bullet Collision Check
         if (collision.gameObject.CompareTag("Bullet"))
         {
             Destroy(this.gameObject);
         }
 
-        //Head collision Check with y value
         if (collision.gameObject.CompareTag("Player") && player.position.y > body.position.y + 1)
         {
             Destroy(this.gameObject);
@@ -85,63 +86,59 @@ public class WalkingEnemyWall : MonoBehaviour
         
     }
 
-    //Finds the difference in x between enemy and player to change velocity of the enemy
+
     private void DistanceCheck()
     {
-        float distance = player.position.x - body.position.x; //Difference between player position and enemy position
-
-        if ((distance < range && distance > -range) && inRange == false)
+        if (play != null)
         {
-            //right check to turn 
-            if (distance > 0)
+            float distance = player.position.x - body.position.x;
+            if ((distance < range && distance > -range) && inRange == false)
             {
-                //if walkingspeed is negative
-                if (walkingSpeed < 0)
+                if (distance > 0)
                 {
-                    walkingSpeed = -walkingSpeed;
-                    direction = true;
 
+                    if (walkingSpeed < 0)
+                    {
+                        walkingSpeed = -walkingSpeed;
+                        direction = true;
+                        
+
+                    }
                 }
-            }
-            //Left check to turn 
-            else if (distance < 0)
-            {
-                //If walking speed is positive
-                if (walkingSpeed > 0)
+                else if (distance < 0)
                 {
-                    walkingSpeed = -walkingSpeed;
-                    direction = false;
+
+                    if (walkingSpeed > 0)
+                    {
+                        walkingSpeed = -walkingSpeed;
+                        direction = false;
+                        
+                    }
                 }
+                checkDist = distance;
+                inRange = true;
             }
-            checkDist = distance;
-            inRange = true;
+            else if (distance < range && distance > -range && inRange == true)
+            {
+                SwitchSides(ref checkDist);
+            }
+            else if (distance > range && distance > -range && inRange == true)
+            {
+                inRange = false;
+            }
         }
-        //in Range hasnt changed but checks if needs to turn
-        else if (distance < range && distance > -range && inRange == true)
-        {
-            SwitchSides(ref checkDist);
-        }
-        //Out of Range Check
-        else if (distance > range && distance > -range && inRange == true)
-        {
-            inRange = false;
-        }
-        
     }
 
-    //Switches sides if still in range but distance has changed from positive to negative and vise versa
     private void SwitchSides(ref float saved)
     {
         float newDistance = player.position.x - body.position.x;
 
-        //distance changed from negative to positive
         if (saved < 0 && newDistance > 0)
         {
             walkingSpeed = -walkingSpeed;
             direction = true;
             saved = newDistance;
-        }
-        //distace changed from postive to negative 
+        }       
         if (saved > 0 && newDistance < 0)
         {
             walkingSpeed = -walkingSpeed;
